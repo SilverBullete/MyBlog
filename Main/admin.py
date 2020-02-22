9d00e93f204658b9440c711f212ed1a4c0741e95
SilverBullete <1256819071@qq.com>
2020-02-22 15:53:46 +0800

    update 美化代码显示

--- Main/admin.py
+++ Main/admin.py
@@ -47,7 +47,8 @@ class BlogAdmin(admin.ModelAdmin):
             src = image.split("src=\"")[1].split("\"")[0]
             filename = "".join(src.split("/")[-1].split("\'")[0].split("%20"))
             content = content.replace(src, "/media/" + str(obj.id) + "/" + filename)
-        content.replace("<code>", "<code class='language-python'>")
+        content = content.replace("<code>", "<code class='language-python'>")
+        content = content.replace("class=\"code\"", "")
         obj.content = content
         super(BlogAdmin, self).save_model(request, obj, form, change)
 
